7ad94fd4a2ee9744ba3c0b552aa4d1efa26877fc
Abhishek Muvvala <abhishekmuvvala@Abhisheks-MacBook-Air.local>
2021-03-17 17:01:30 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css';
+import Header from './components/Header';
+import GameBoard from './components/GameBoard';
+
+function App() {
+  return (
+    <div className="App">
+     <Header />
+     <GameBoard />
+
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="text-center header shadow-sm p-2 mb-5">
+         <h1>Tic Tac Toe</h1>
+        </div>
+      );
+}
+ 
+export default Header
